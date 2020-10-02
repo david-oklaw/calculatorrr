5e4af5310576151ea0a80d18a5fa1e27a9916f21
david-oklaw <72260878+david-oklaw@users.noreply.github.com>
2020-10-02 15:55:43 +0300

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,21 @@
+first_number = float(input("Enter the first number "))
+second_number = float(input("Enter the second number "))
+summation = first_number+second_number
+difference1 = first_number-second_number
+difference2 = second_number-first_number
+product = first_number*second_number
+quotient1 = first_number/second_number
+quotient2 = second_number/first_number
+print("\n" + "Your results are as follows;")
+print("............................")
+print(str(first_number) + " + " + str(second_number) +" = " + str(summation))
+print(str(first_number) + " - " + str(second_number) +" = " + str(difference1))
+print(str(second_number) + " - " + str(first_number) +" = " + str(difference2))
+print(str(first_number) + " * " + str(second_number) +" = " + str(product))
+print(str(first_number) + " / " + str(second_number) +" = " + str(quotient1))
+print(str(second_number) + " / " + str(first_number) +" = " + str(quotient2))
+print("\n" + "Thank you for using our calculator.")
+print("***********************************")
+
+
+
